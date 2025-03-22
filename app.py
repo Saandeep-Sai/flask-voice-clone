6436e294d97f2222bca759c2eacee409f288f373
Saandeep-Sai <160743940+Saandeep-Sai@users.noreply.github.com>
2025-03-22 06:36:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -287,5 +287,5 @@ def serve_output(filename):
         return jsonify({"error": "File not found"}), 404
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
